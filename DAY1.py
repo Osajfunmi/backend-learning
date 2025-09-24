77872648bdbe1818acbd1090a4f95148ebb2aa59
Olufunmilayo Osajuyigbe <osajuyigbefunmi@gmail.com>
2025-09-24 11:29:17 +0100

    First commit - setup backend learning

---
+++ DAY1.py
@@ -0,0 +1,15 @@
+# day1c.py
+
+# Ask the user for two numbers
+num1 = input("Enter first number: ")
+num2 = input("Enter second number: ")
+
+# Convert the inputs to integers
+num1 = int(num1)
+num2 = int(num2)
+
+# Add the numbers
+sum_result = num1 + num2
+
+# Display the result
+print("The sum of", num1, "and", num2, "is:", sum_result)
